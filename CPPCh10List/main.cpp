3aa99b8abe2057d6553d3264f99facae32831a3d
Dave Reed <dave256@mac.com>
2023-09-07 07:38:32 -0400

    Initial Commit

---
+++ CPPCh10List/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPPCh10List
+//
+//  Created by David Reed on 9/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
